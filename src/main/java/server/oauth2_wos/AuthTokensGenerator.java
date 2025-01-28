3cbaa9ffaec57fc314a0abb085522a9372708ceb
Etwashoeren <sungmin0609@nate.com>
2025-01-29 01:54:40 +0900

    feat. AccessToken 발급 로직 추가

---
+++ src/main/java/server/oauth2_wos/AuthTokensGenerator.java
@@ -0,0 +1,32 @@
+package server.oauth2_wos;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+@RequiredArgsConstructor
+public class AuthTokensGenerator { // AccessToken 발급 로직
+    private static final String BEARER_TYPE = "Bearer";
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30;
+    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public AuthTokens generate(Long memberId) {
+        long now = (new Date()).getTime();
+        Date accessTokenExpiredAt = new Date(now + ACCESS_TOKEN_EXPIRE_TIME);
+        Date refreshTokenExpiredAt = new Date(now + REFRESH_TOKEN_EXPIRE_TIME);
+
+        String subject = memberId.toString();
+        String accessToken = jwtTokenProvider.generateToken(subject, accessTokenExpiredAt);
+        String refreshToken = jwtTokenProvider.generateToken(subject, refreshTokenExpiredAt);
+
+        return AuthTokens.of(accessToken, refreshToken, BEARER_TYPE, ACCESS_TOKEN_EXPIRE_TIME / 1000L);
+    }
+
+    public Long extractMemberId(String accessToken) {
+        return Long.valueOf(jwtTokenProvider.extractSubject(accessToken));
+    }
+}
