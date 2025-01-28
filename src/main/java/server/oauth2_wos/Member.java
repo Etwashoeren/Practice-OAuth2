8ac855be684cc9177be77a92e685f5e57ebc5d53
Etwashoeren <sungmin0609@nate.com>
2025-01-29 00:54:29 +0900

    feat. Member 엔티티 추가

---
+++ src/main/java/server/oauth2_wos/Member.java
@@ -0,0 +1,33 @@
+package server.oauth2_wos;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Member {// 회원 정보를 담는 엔티티
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String nickname;
+
+    private OAuthProvider oAuthProvider;
+
+    @Builder
+    public Member(String email, String nickname, OAuthProvider oAuthProvider) {
+        this.email = email;
+        this.nickname = nickname;
+        this.oAuthProvider = oAuthProvider;
+    }
+
+}
