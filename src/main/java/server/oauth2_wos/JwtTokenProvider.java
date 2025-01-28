cef45dd3b1c33b561e0267d73af3261b7c7c378e
Etwashoeren <sungmin0609@nate.com>
2025-01-29 01:48:12 +0900

    feat. JwtTokenProvider 추가

---
+++ src/main/java/server/oauth2_wos/JwtTokenProvider.java
@@ -0,0 +1,47 @@
+package server.oauth2_wos;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    private final SecretKey secretKey;
+
+    public JwtTokenProvider(@Value("${jwt.secret-key}") String secretKey) {
+        this.secretKey = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+    }
+
+    public String generateToken(String subject, Date expiredAt) {
+        return Jwts.builder()
+                .subject(subject)
+                .expiration(expiredAt)
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public String extractSubject(String accessToken) {
+        Claims claims = parseClaims(accessToken);
+        return claims.getSubject();
+    }
+
+    private Claims parseClaims(String accessToken) {
+        try{
+            return Jwts.parser()
+                    .verifyWith(secretKey)
+                    .build()
+                    .parseSignedClaims(accessToken)
+                    .getPayload();
+        } catch (ExpiredJwtException e) {
+            return e.getClaims();
+        }
+    }
+}
