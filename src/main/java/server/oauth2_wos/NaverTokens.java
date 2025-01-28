af1a754f3062790d2940c6925f364aefe647fc0c
Etwashoeren <sungmin0609@nate.com>
2025-01-29 01:05:59 +0900

    feat. NaverTokens 추가

---
+++ src/main/java/server/oauth2_wos/NaverTokens.java
@@ -0,0 +1,23 @@
+package server.oauth2_wos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class NaverTokens { // 네이버 요청 결과값을 토대로 만든 클래스
+
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+
+    @JsonProperty("token_type")
+    private String tokenType;
+
+    @JsonProperty("expires_in")
+    private String expiresIn;
+
+}
