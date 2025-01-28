8e5f525e6e8fc6e34af0f51492081583a52c75a8
Etwashoeren <sungmin0609@nate.com>
2025-01-29 00:46:54 +0900

    feat. 초기 세팅

---
+++ src/main/java/server/oauth2_wos/OAuth2WoSApplication.java
@@ -0,0 +1,13 @@
+package server.oauth2_wos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2WoSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuth2WoSApplication.class, args);
+    }
+
+}
---
+++ src/test/java/server/oauth2_wos/OAuth2WoSApplicationTests.java
@@ -0,0 +1,13 @@
+package server.oauth2_wos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2WoSApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
