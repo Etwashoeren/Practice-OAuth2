a5b0c9412596d25a9827ba0d50a19cec66d53d89
Etwashoeren <sungmin0609@nate.com>
2025-01-29 00:51:29 +0900

    feat. ClientConfig 클래스 작성

---
+++ src/main/java/server/oauth2_wos/ClientConfig.java
@@ -0,0 +1,15 @@
+package server.oauth2_wos;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ClientConfig { //RestTemplate를 사용하기 위한 클래스
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
