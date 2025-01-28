92d897c387f9594d0fe0d5b979afd2bd808e95df
Etwashoeren <sungmin0609@nate.com>
2025-01-29 00:59:41 +0900

    feat. KakaoLoginParams 추가

---
+++ src/main/java/server/oauth2_wos/KakaoLoginParams.java
@@ -0,0 +1,25 @@
+package server.oauth2_wos;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+@Getter
+@NoArgsConstructor
+public class KakaoLoginParams implements OAuthLoginParams { // 카카오 API 요청에 필요한 authorizationCode를 가지고 있는 클래스
+
+    private String authorizationCode;
+
+    @Override
+    public OAuthProvider oAuthProvider() {
+        return OAuthProvider.KAKAO;
+    }
+
+    @Override
+    public MultiValueMap<String, String> makeBody() {
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("code", authorizationCode);
+        return body;
+    }
+}
