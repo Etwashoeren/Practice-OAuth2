cc71427fdccdc8a3eb816a8d6701ca7604a03bb5
Etwashoeren <sungmin0609@nate.com>
2025-01-29 01:16:35 +0900

    feat. OAuthClient 추가

---
+++ src/main/java/server/oauth2_wos/OAuthClient.java
@@ -0,0 +1,7 @@
+package server.oauth2_wos;
+
+public interface OAuthClient { // OAuth 요쳥을 위한 클래스
+    OAuthProvider oAuthProvider(); // Client의 타입 반환
+    String requestAccessToken(OAuthLoginParams params); // AccessToken을 획득
+    OAuthInfoResponse requestOauthInfo(String accessToken); // AccessToken을 기반으로 프로필 정보 획득
+}
