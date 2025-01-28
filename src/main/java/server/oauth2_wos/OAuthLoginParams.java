487abeac371ba03bcf7a7b2686591d1918db25e0
Etwashoeren <sungmin0609@nate.com>
2025-01-29 00:59:15 +0900

    feat. OAuthLoginParams 추가

---
+++ src/main/java/server/oauth2_wos/OAuthLoginParams.java
@@ -0,0 +1,8 @@
+package server.oauth2_wos;
+
+import org.springframework.util.MultiValueMap;
+
+public interface OAuthLoginParams { // OAuth 요청을 위한 파라미터 값들을 갖고 있는 인터페이스
+    OAuthProvider oAuthProvider();
+    MultiValueMap<String, String> makeBody();
+}
