fc4c0065a0f44d07007d6ca53166c894975352c8
Etwashoeren <sungmin0609@nate.com>
2025-01-29 01:57:33 +0900

    feat. MemberRepository 추가

---
+++ src/main/java/server/oauth2_wos/MemberRepository.java
@@ -0,0 +1,7 @@
+package server.oauth2_wos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Member findByEmail(String email);
+}
